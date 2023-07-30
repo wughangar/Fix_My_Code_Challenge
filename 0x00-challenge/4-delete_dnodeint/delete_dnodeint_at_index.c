d7df873e041b0ccff4dfa8b4cdf13dd1d7cae4bd
wughangar <wughangar.rose@gmail.com>
2023-07-30 16:48:11 +0300

    delte node

---
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ *
+ * @head: A pointer to the first element of a list
+ * @index: The index of the node to delete
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+    dlistint_t *current = *head;
+    unsigned int i;
+
+    if (*head == NULL)
+        return (-1);
+
+    if (index == 0)
+    {
+        *head = (*head)->next;
+        if (*head != NULL)
+            (*head)->prev = NULL;
+        free(current);
+        return (1);
+    }
+
+    for (i = 0; current != NULL && i < index; i++)
+    {
+        current = current->next;
+    }
+
+    if (current == NULL)
+        return (-1);
+
+    current->prev->next = current->next;
+    if (current->next != NULL)
+        current->next->prev = current->prev;
+
+    free(current);
+    return (1);
+}
