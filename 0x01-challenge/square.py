c139546d139b2d040112f17847f244b881630ad5
wughangar <wughangar.rose@gmail.com>
2023-09-14 08:58:42 +0000

    square

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+class square():
+    def __init__(self, width=0, height=0, **kwargs):
+        super().__init__(**kwargs)
+        self.width = width
+        self.height = height
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def permiter_of_my_square(self):
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        return "{}/{}".format(self.width, self.height)
+
+if __name__ == "__main__":
+
+    s = square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
